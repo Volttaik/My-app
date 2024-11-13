1dccd7d86657102130dbda7c2d547502d50eae19
devatron <onyeaghorlouis@gmail.com>
2024-11-13 05:28:49 +0100

    Update app with new changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App!</h1>
+        <h1>By Devatron!</h1>
         <p>This is your custom app, ready to be deployed and installed.</p>
       </header>
     </div>
