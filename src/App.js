e4c7f063b41e127fd5837f7aa934ee6db45057ad
devatron <onyeaghorlouis@gmail.com>
2024-11-13 06:22:46 +0100

    Update app with new changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a 
-          href="/PHP-PDO-SQLite-1%20(1)/index.php" 
-          target="_blank" 
+        {/* Link to the folder, which is now the main page */}
+        <a
+          href="/PHP-PDO-SQLite-1%20(1)/"
+          target="_blank"
           rel="noopener noreferrer"
         >
-          Open PHP-PDO-SQLite-1 (1) Index
+          Open PHP-PDO-SQLite-1 (1) Folder
         </a>
       </header>
     </div>
