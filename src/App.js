0f0b9cb27dffb5af2e56a84edaeaeccaea6f8987
devatron <onyeaghorlouis@gmail.com>
2024-11-13 05:04:15 +0100

    Initial commit with project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>This is your custom app, ready to be deployed and installed.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
